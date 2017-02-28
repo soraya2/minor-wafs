50c17a24151e3294d6805e4ec893f1c0aba077ba
Soraya Ibrahim <soraya_ibrahim@live.nl>
2017-03-01 00:16:38 +0100

    rewrote all objects and functions to mvc model

--- week2/rijksmuseumApi/static/js/data.js
+++ week2/rijksmuseumApi/static/js/data.js
@@ -1,146 +1,179 @@
 // url: "https://www.rijksmuseum.nl/api/nl/collection/?key=LTaH2LtF&format=json&q=rembrand"
-
+// on submit spinner starten en pas stoppen als de data is geladen.
+//https://developer.mozilla.org/nl/docs/Web/API/Document/readyState
 (function() {
 
     'use strict';
 
-    var config, getData, userEventRespond;
-    var getPaintingOverview = false;
-    var form = document.querySelector('.search-form');
-    var htmlContent = '';
+    var config = {
+      baseUrl: 'https://www.rijksmuseum.nl/api/nl/collection/'
 
-
-    config = {
-        userInput: document.getElementById('userInput'),
-        button: document.getElementById('submit'),
-        url: 'https://www.rijksmuseum.nl/api/nl/collection/?key=LTaH2LtF&format=json&q=',
-        baseUrl: 'https://www.rijksmuseum.nl/api/nl/collection/',
-        _key: '?key=LTaH2LtF',
-        html: ''
     };
 
-    var routes ={
+
+    var app = {
       init: function(){
-          routie('details');
+        controller.routes();
+        controller.searchInput();
+
       }
     };
 
-    getData = {
-      details: function(paintingNumber){
-         aja()
-            .url(config.baseUrl+paintingNumber+config._key+"&format=json")
-
-            .on('success', function(info) {
-              var artworkInfo = info.artObject;
-              // console.log(artworkInfo);
-
-              function returnHtml(){
-
-                 function imageAvailable(){
-                      if (artworkInfo.webImage !== null){
-                          return artworkInfo.webImage.url;
-                      }else{
-                        return"./static/images/background_black.svg";
-                      }
-                    }
-                 function descriptionAvailable(){
-                    if (artworkInfo.description!== null) {
-                          return artworkInfo.description;
-                      }else{
-                        return"Geen beschrijving beschikbaar";
-                      }
-                    }
-
-               var source = document.getElementById("detail-template").innerHTML;//javascipt template
-                    //content
-                    var content = {
-                      artMaker : artworkInfo.principalOrFirstMaker,
-                      longTitle: artworkInfo.longTitle,
-                      artImgUrl: imageAvailable(),
-                      artDescription: descriptionAvailable()
-                    }
-                    //compile content naar html
-                    var template = Handlebars.compile(source);
-                    var htmlContent = template(content);
-
-                    return htmlContent
-
-                  // return  `
-                  //   <section class="item2">
-                  //       <h1>${artworkInfo.longTitle}</h1>
-                  //       <img src = ${imageAvailable()}></img>
-                  //       <p>${descriptionAvailable()}</p>
-                  //   </section>
-                  // `;
-
-}
 
 
-              document.getElementById('details').innerHTML = returnHtml();
-            })
-            .go();
-      },
+    //controller responsible for user events
+    var controller = {
+      routes: function(){
+            routie({
+                '': function() {
+                      location.hash = '#home';
+                },
+                'home': function(){
 
-      overview: function(searchQuery) {
-          aja()
-            .url(config.baseUrl+config._key+"&format=json&q="+ searchQuery)
+                },
 
-            .on('success', function(data) {
-
-                  // < array
-
-                    // Verander elk object naar een string > <div>Naam</div>
-
-                    function imageCheck(){
-
-                        if (webImage !== null) {
-                          return webImage.url;
-
-                        }else {
-                          return"./static/images/background_black.svg";
-
-                        }
-                    }
-
-                    var source = document.getElementById("overview-template").innerHTML;//javascipt template
-                    //content
-
-                    var template = Handlebars.compile(source);
-                    var htmlContent = template(data);
-
-                   document.getElementById('painting-overview').innerHTML = htmlContent;
-
-                  // Voeg elke string samen tot 1 grote string > <div>Naam</div><div>Naam</div>
-
-                  getPaintingOverview=true;
-                  userEventRespond.artworkDetail(document.getElementById('painting-overview'));
-            })
-            .go();
-        }
-    };
-
-    userEventRespond = {
-        init: function() {
-            form.addEventListener('submit', function(event){
-                event.preventDefault();
-                var input = config.userInput.value;
-                getData.overview(input);
+                'art/:id': function(id) {
+                    console.log('details');
+                }
             });
+          },
+
+      searchInput: function(){
+          var form = document.querySelector('.search-form');
+          var userInput = document.getElementById('userInput');
+
+          form.addEventListener('submit', function(event){
+
+
+              if(userInput.value !==''){
+
+                event.preventDefault();
+                var input = userInput.value;
+                model.getPaintings(input);
+              }
+          });
         },
 
-        artworkDetail: function (selectPaintingOverview){
+      getPaintingId: function(selectPaintingOverview){
 
-            if (getPaintingOverview === true){
-              selectPaintingOverview.addEventListener('click', function(event){
+          selectPaintingOverview.addEventListener('click', function(event){
                 event.preventDefault();
+                console.log(event.target.parentElement.id);
 
-                getData.details(event.target.parentElement.id);
-                routes.init();
-              });
-            }
-          }
+                model.getPaintingDetails(event.target.parentElement.id);
+                controller.routes();
+          });
+
+      }
     };
 
-    userEventRespond.init();
+
+
+
+    //model resposible for storing data
+    var model = {
+      getPaintings: function(searchQuery) {
+
+       aja()
+            .url(config.baseUrl+"?key=LTaH2LtF&format=json&q="+ searchQuery)
+
+            .on('success', function(data) {
+                view.paintingsOverview(data);
+            })
+
+            .go();
+
+      },
+
+      getPaintingDetails: function(paintId) {
+
+         aja()
+            .url(config.baseUrl+paintId+"?key=LTaH2LtF&format=json")
+
+            .on('success', function(info) {
+
+              var artworkInfo = info.artObject;
+              view.paintingDetails(artworkInfo);
+
+            })
+            .go();
+
+      },
+
+      // save:function() {
+
+
+      // }
+
+    };
+    //responsible for rendering html
+
+    var view = {
+      paintingsOverview: function(data){
+         function imageCheck(){
+
+            if (webImage !== null) {
+              return webImage.url;
+
+            }else {
+              return"./static/images/background_black.svg";
+
+            }
+        }
+
+        var source = document.getElementById("overview-template").innerHTML;//javascipt template
+        //content
+
+        var template = Handlebars.compile(source);
+        var htmlContent = template(data);
+
+       document.getElementById('painting-overview').innerHTML = htmlContent;
+       controller.getPaintingId(document.getElementById('painting-overview'));
+
+      },
+
+      paintingDetails: function(artworkInfo){
+         function returnHtml(){
+
+           function imageAvailable(){
+                if (artworkInfo.webImage !== null){
+                    return artworkInfo.webImage.url;
+                }else{
+                  return"./static/images/background_black.svg";
+                }
+              }
+           function descriptionAvailable(){
+              if (artworkInfo.description!== null) {
+                    return artworkInfo.description;
+                }else{
+                  return"Geen beschrijving beschikbaar";
+                }
+              }
+
+         var source = document.getElementById("detail-template").innerHTML;//javascipt template
+              //content
+              var content = {
+                artMaker : artworkInfo.principalOrFirstMaker,
+                longTitle: artworkInfo.longTitle,
+                artImgUrl: imageAvailable(),
+                artDescription: descriptionAvailable()
+              };
+              //compile content naar html
+              var template = Handlebars.compile(source);
+              var htmlContent = template(content);
+
+              return htmlContent;
+          }
+          document.getElementById('details').innerHTML = returnHtml();
+
+      },
+
+      toggle: function(){
+
+
+      }
+    };
+
+    app.init();
 
 }());
