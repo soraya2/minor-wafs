15eb6e55f0838efbaa30b5aed1ec49a7cb9eb8af
soraya2 <soraya_ibrahim@live.nl>
2017-05-08 20:58:18 +0200

    added  changes

---
+++ eindopdracht/readme.md
@@ -0,0 +1,2 @@
+#Week 2 asignment
+https://soraya2.github.io/minor-wafs/week2/rijksmuseumApi/index.html
---
+++ eindopdracht/rijksmuseumapi/static/js/app.js
@@ -0,0 +1,51 @@
+// (function(){
+
+//     'use strict';
+//     window.location.hash = '#home';
+//     var getSections = document.querySelectorAll('main .content');
+
+//     var app = {
+//         init: function() {
+//             routes.init();
+//         }
+//     };
+
+//     var routes = {
+//         init: function() {
+//             var startHash = window.location.hash;
+
+//             if (startHash==="#home"){
+//               // console.log(getSections.children);
+//               // sections.toggle(startHash);
+//             }
+//             // check if hash value is empty otherwise just use the hash value
+//             // if (window.location.hash==='') {
+//             //   return startHash = 'home';
+//             // }
+
+//             //Add event hashchange listener to toggle function
+//             window.addEventListener('hashchange', function() {
+//                 sections.toggle(location.hash.split('#')[1]);
+//             });
+//         }
+//     };
+
+//     //Get all sections and add class if selected link is equal to section id
+//     var sections = {
+
+//         toggle: function(route) {
+//             var selectSection,sectionsId, i;
+//             console.log("toggle")
+
+//             for (i=0; i < getSections.length; i++) {
+//               selectSection = getSections[i];
+//               sectionsId = getSections[i].id;
+
+//               (sectionsId === route) ? selectSection.classList.remove('hide') : selectSection.classList.add('hide');
+//             }
+//         }
+//     };
+
+//    app.init();
+
+// }());
